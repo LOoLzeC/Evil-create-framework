39da314235e3597a4f870d234728b328ed7e6643
LOoLzeC <33969267+LOoLzeC@users.noreply.github.com>
2018-02-04 02:46:02 +0700

    Add files via upload

---
+++ documents/macosx.py
@@ -0,0 +1,13 @@
+R = '\033[31m' # Red
+N = '\033[1;37m' # White
+def a():
+	print ""+N+""
+	print "VIRUS FOR MACOSX COLLECTIONS"
+	print "============================="
+	print 
+	print "name                                        rank"
+	print "----                                        ----\033[31m"
+	print "/modules/macosx/module_trinoids             not tested"
+	print "/modules/macosx/module_nothing              not tested"
+if __name__ == "__main__":
+	a()
---
+++ documents/windows.py
@@ -0,0 +1,23 @@
+R = '\033[31m' # Red
+N = '\033[1;37m' # White
+def a():
+	print ""+N+""
+	print "VIRUS FOR WINDOWS COLLECTIONS"
+	print "============================="
+	print 
+	print "name                                        rank"
+	print "----                                        ----\033[31m"
+	print "/modules/windows/module_req_eater           normal"
+	print "/modules/windows/module_quis                normal"
+	print "/modules/windows/module_ugly                normal"
+	print "/modules/windows/module_sleepy              normal"
+	print "/modules/windows/module_rip                 normal"
+	print "/modules/windows/module_koce                normal"
+	print "/modules/windows/module_capslock            funny :v"
+	print "/modules/windows/module_rdc                 normal"
+	print "/modules/windows/module_alay                normal"
+	print "/modules/windows/module_zipbomb             low"
+	print "/modules/windows/module_cmd                 normal"
+	
+if __name__ == "__main__":
+	a()
